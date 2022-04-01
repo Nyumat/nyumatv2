872882711f7d4cab15c0e185b721384ff1bf0a08
Thomas <nyumat18@gmail.com>
2022-04-01 04:51:21 -0700

    ++Object-Oriented Cookie

--- docs/scripts/cookies.js
+++ docs/scripts/cookies.js
@@ -1,73 +1,43 @@
 
-class randomCookieGenerator {
-      constructor(name, value, days) {
-            this.name = name;
-            this.value = value;
-            this.days = days;
+class Cookie {
+      constructor(length, expiration_days) {
+            this.value = length;
+            this.expiration_days = expiration_days;
       }
-      generateRandomName() {
+
+      generateCode() {
             var text = "";
             var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < this.value; i++)
                   text += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
             return text;
       }
-      generateRandomNumber() {
-            var text = "";
-            var possible_nums = "0123456789";
-            var possible_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            for (var i = 0; i < 5; i++) {
-                  if (i % 2 == 0) {
-                        text += possible_nums.charAt(Math.floor(Math.random() * possible_nums.length));
-                  } else {
-                        text += possible_chars.charAt(Math.floor(Math.random() * possible_chars.length));
-                  }
-            }
-            return text;
-      }
+
       generateExpirationDate(length_days) {
             let d = new Date();
             d.setTime(d.getTime() + (length_days * 24 * 60 * 60 * 1000));
             return d.toUTCString();
       }
 
-}
+      setCookie(name, value, length_days) {
+            document.cookie = name + "=" + value + "; expires=" + this.generateExpirationDate(length_days) + "; path=/";
+      }
 
-class Cookie {
-      constructor(name, value, days) {
-            this.name = name;
-            this.value = value;
-            this.days = days;
-      }
-      set() {
-            let d = new Date();
-            d.setTime(d.getTime() + (this.days * 24 * 60 * 60 * 1000));
-            let expires = "expires=" + d.toUTCString();
-            document.cookie = this.name + "=" + this.value + ";" + expires + ";path=/";
-      }
-      get() {
-            let name = this.name + "=";
-            let ca = document.cookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                  let c = ca[i];
-                  while (c.charAt(0) == ' ') {
-                        c = c.substring(1);
-                  }
-                  if (c.indexOf(name) == 0) {
-                        return c.substring(name.length, c.length);
-                  }
+      getCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                  var c = ca[i];
+                  while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+                  if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
             }
-            return "";
-      }
-      delete() {
-            this.set();
+            return null;
       }
 
+      deleteCookie(name) {
+            this.setCookie(name, "", -1);
+      }
 }
 
-var cookie = new Cookie("test", "test", 1);
-cookie.set();
-alert(cookie.get());
-cookie.delete();
-alert(cookie.get());
+
+
