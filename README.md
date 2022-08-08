6a8f4526a7850ae2f10981dbe7882528357c163e
Thomas <nyumat18@gmail.com>
2022-08-07 19:44:39 -0700

    Reverted

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center" style="font-size: 3rem;">Nyumat's Website Repository</h1>
+
+## The Project
+
+<p align="center" style="font-size: 3rem;">Repository dedicated to the personal website I'll be using to host my entire CS290 coursework.</p>
+
+<p align="center" style="font-size: 3rem;">Update: Adding a blog!</p>
+
+<p align="center" style="font-size: 3rem;">Let's get better, together!</p>
+
+## Specs 
+
+- FontAwesome / GoogleFonts / [Animate on Scroll](https://github.com/michalsnik/aos)
+- jQuery / HTML5 / CSS / jS
+
+# To-Do's:
+
+New To-Do's coming soon.
+
+# License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
---
+++ docs/scripts/loading.js
@@ -0,0 +1,50 @@
+// loading.js 
+
+const hideSplash = () => {
+      if (Cookies.get("Loading Screen") == "Seen") {
+            $(".load").hide();
+      } 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+      hideSplash();
+      AOS.init({
+            once: false,
+            mirror: true,
+            duration: 1500,
+            anchorPlacement: "top-bottom",
+      });
+});
+
+/**
+ * @param {integer} count_to - The number to count to. (required)
+ * 
+ */
+function onsetload(count_to) {
+      var tenMinutesFromNow = new Date(new Date().getTime() + 10 * 60 * 1000);
+      const status = Cookies.get("Loading Screen");
+      if (status == "Seen") {
+            $(".post_load").fadeIn("slow");
+      } else {
+            var start_count = 0;
+            var counter = setInterval(function () {
+                  $(".counter").text(start_count + "%");
+                  start_count += 2;
+                  $(".counter").css("font-size", "+=0.7px");
+                  if (start_count == count_to) {
+                        clearInterval(counter);
+                        $(".counter").text(count_to + "%");
+                        setTimeout(function () {
+                              $(".loader-inner").fadeOut(500);
+                              $(".counter").fadeOut(500);
+                              setTimeout(function () {
+                                    $(".post_load").fadeIn(500);
+                              }, 500);
+                        }, 500);
+                  } 
+            }, 80);
+            Cookies.set("Loading Screen", "Seen", {
+                  expires: tenMinutesFromNow
+            });
+      }
+}
