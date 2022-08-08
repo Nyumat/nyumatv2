ddfecf3d0f8ae45db1d9df8a51ff7aa7a04cc70f
Thomas <nyumat18@gmail.com>
2022-08-07 19:06:55 -0700

    V2 Published.

---
+++ docs/src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "TOMNYUMA";
+const meta = {
+    title: "Tom Nyuma",
+    description: "I'm Tom Nyuma, student, developer, and musician from Portland, Oregon.",
+};
+
+const introdata = {
+    title: "I'm Tom Nyuma",
+    animated: {
+        first: "I develop cool applications.",
+        second: "I make and perform music.",
+        third: "Coding is a hobby of mine.",
+        fourth: "This is what I look like -------->",
+    },
+    description: "I'm a student, engineer, and activist. Look around my website for more about me and what I've been up to! :-) ",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "This is my second year studying computer science at Oregon State University (go beavs). I'm apart of National Society of Black Engineers and Association of Computing Machinery. I also do marching band, spending weekends cheering on our sports teams and being full of school spirit.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developement Engineer Intern",
+        where: "Amazon",
+        date: "Summer 2022",
+    },
+    {
+        jobtitle: "Fullstack Engineering Intern",
+        where: "BountyJobs",
+        date: "Fall 2021 - Spring 2022",
+    },
+    {
+        jobtitle: "STEP Intern",
+        where: "Google",
+        date: "Summer 2021",
+    },
+];
+
+const skills = [{
+        name: "Web Development/Frontend",
+        value: 90,
+    },
+    {
+        name: "Embed Systems/Robotics",
+        value: 85,
+    },
+    {
+        name: "Backend",
+        value: 75,
+    },
+    {
+        name: "Databases",
+        value: 60,
+    },
+    {
+        name: "Architecture/Design",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "Reading Manga and watching Anime",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Playing Clarinet and Euphonium",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Basketball, Lacrosse, and Soccer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://camo.githubusercontent.com/7baadd7f1b99025d039dd13f3166f72692f0aad31745f63c9ec8cb97f7b3e343/68747470733a2f2f747269636f6c6f7274696d65732e636f6d2f77702d636f6e74656e742f75706c6f6164732f323032302f30322f526576696577735f4c617273656e2d576869746e69655f54696b546f6b2e737667",
+        desctiption: "An bot created to help creators programmatically generate views/shares on their content.",
+        link: "https://github.com/Nyumat/The-TikTok-Bot",
+    },
+    {
+        img: "https://camo.githubusercontent.com/df251832c062a3e6dcb1703db2e3979631bfb106280967e9747e07b9d83e8b65/68747470733a2f2f692e6962622e636f2f574432637752592f6c6f676f2e706e67",
+        desctiption: "An open-sourced, free movie and television streaming platform.",
+        link: "http://www.nyumat.tech/",
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/440px-Twitter-logo.svg.png",
+        desctiption: "A bot that tweets artist' J. Cole lyrics every 20 minutes.",
+        link: "https://github.com/Nyumat/Twitter-J.Cole-Bot",
+    },
+    {
+        img: "https://assets.website-files.com/6072096d69322257bcddeb3c/6088c844759e9581ae8354de_tinder.jpeg",
+        desctiption: "Tinder, but for movies and tv shows. Match and swipe to find your next favorite.",
+        link: "https://github.com/CS290-Final-Project-Team-24/TinderForMoviesV2",
+    },
+    {
+        img: "https://www.pygame.org/ftp/pygame-head-party.png",
+        desctiption: "An A* Visualization Tool written in Python.",
+        link: "https://github.com/Nyumat/Pathfinding-Algorithm-Tool",
+    },
+    {
+        img: "https://bignerdranch.com/wp-content/uploads/2021/03/SwiftUI-Logo-copy.jpg",
+        desctiption: "A SwiftUI News Application that pulls real world News headlines and information from the NewsAPI.",
+        link: "https://github.com/Nyumat/TEKNews",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nyumat18@gmail.com",
+    YOUR_FONE: "239-285-5148",
+    description: "Are you a recruiter or employer? I'm always looking for new opportunities! Contact me and I'll get back to you within 24 hours!",
+    YOUR_SERVICE_ID: "service_n49lq2b",
+    YOUR_TEMPLATE_ID: "template_h6hm5ik",
+    YOUR_USER_ID: "nHcnP2sA7_J8KmIRp",
+};
+
+const socialprofils = {
+    github: "https://github.com/nyumat",
+    facebook: "https://www.facebook.com/tnyuma.02/",
+    linkedin: "https://linkedin.com/nyumat",
+    twitter: "https://twitter.com/tomnyuma",
+    youtube: "https://www.youtube.com/channel/UCbhv7YNL_wn-b6yPQ9luw2Q/",
+    twitch: "https://twitch.com",
+    instagram: "https://instagram.com/tomnyuma",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
